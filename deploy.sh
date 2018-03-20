f52e1bc1f4200558b83ddd97363cb28f45b65d97
Fayland Lam <fayland@gmail.com>
2018-03-20 22:13:27 +0800

    fix time

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 rm -rf _book
 gitbook build
 rm -rf ~/bsportsfan.com/public/docs
+
+# We really do not care about the build time
+find _book -name "*.html" | xargs perl -i -pe 's/,"time":".*?"//;'
+
 cp -Rvf _book ~/bsportsfan.com/public/docs
